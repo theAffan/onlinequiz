8345dd35a044e284355aeadc318a19f1fec16847
Affan <94933585+theAffan@users.noreply.github.com>
2024-06-17 18:56:54 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is JavaScript primarily used for?",
+    answer: "Creating interactive web elements",
+    options: [
+      "Styling web pages",
+      "Creating interactive web elements",
+      "Structuring web content",
+      "Managing server-side operations"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the difference between the Internet and an intranet?",
+    answer: "One is public, the other is private",
+    options: [
+      "One is public, the other is private",
+      "One is safer than the other",
+      "One can be monitored, the other can't",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Computers calculate numbers in what mode?",
+    answer: "Binary",
+    options: [
+      "Decimal",
+      "Octal",
+      "Binary",
+      "None of the above"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
